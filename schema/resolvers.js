bbbd0ca4681101f3686edeaaf561f36025e7fc19
jotas <jotasenator@gmail.com>
2022-02-13 20:59:07 -0500

    first commit

---
+++ schema/resolvers.js
@@ -0,0 +1,32 @@
+const { UserList, MovieList } = require("./fakeData")
+const _ = require("lodash")
+
+
+
+const resolvers = {
+    Query:{
+        users:()=>{
+             return UserList;
+        },      
+        user:(parent,args)=>{
+            const id = args.id;
+            const user = _.find(UserList,{id:Number(id)});
+            return user
+        },
+        movies:()=>{
+            return MovieList
+        },
+        movie:(parent,args)=>{
+            const name = args.name;
+            const movie = _.find(MovieList,{name});
+            return movie             
+        }  
+    },
+    User:{
+        favoriteMovies: ()=>{
+            return _.filter(MovieList,(movie)=>movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010)
+        }
+    }
+}
+
+module.exports = {resolvers}
