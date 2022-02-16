247aa4cfc2a65d2c0e904e9ee3921d7c0b8f869e
jotas <jotasenator@gmail.com>
2022-02-16 08:53:43 -0500

    Create Display data fetching modify

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
 import {ApolloClient,InMemoryCache,ApolloProvider} from "@apollo/client";
+import DisplayData from './DisplayData';
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri:"http://localhost:4000/graphql"
+  })
   return (
-    <div className="App">
-      
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+       <DisplayData/>
+        
+      </div>
+    </ApolloProvider>
   );
 }
 
